c4b504a7e381f5a4597eafdf558366126bcfe072
Toomas <toomas.heljula@khk.ee>
2022-05-11 16:15:01 +0300

    Sort the result alphabetically by name:

--- db.js
+++ db.js
@@ -7,9 +7,10 @@ var con = mysql.createConnection({
     database:"mydb"
 });
 
+
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers WHERE aadress LIKE 'S%'", function (err, result) {
+    con.query("SELECT * FROM customers ORDER BY name", function (err, result) {
         if (err) throw err;
         console.log(result);
     });

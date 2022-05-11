bd9b59d09d335fdea6dfde6168772039b96a1aca
Toomas <toomas.heljula@khk.ee>
2022-05-11 16:14:00 +0300

    Select records where the address starts with the letter 'S':

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers WHERE aadress = 'Park Lane 38'", function (err, result) {
+    con.query("SELECT * FROM customers WHERE aadress LIKE 'S%'", function (err, result) {
         if (err) throw err;
         console.log(result);
     });

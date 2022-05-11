112e65e6a766551c67765b2d9079fe7fae52c0b7
Toomas <toomas.heljula@khk.ee>
2022-05-11 16:08:01 +0300

    Insert into

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "INSERT INTO mydb (name, address) VALUES ('Company Inc', 'Highway 37')";
+    var sql = "INSERT INTO customers (name, aadress) VALUES ('Company Inc', 'Highway 37')";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");

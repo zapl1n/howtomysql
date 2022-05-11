df8aba2e8f4a23c4e7a7e26e5504bd092119d488
Toomas <toomas.heljula@khk.ee>
2022-05-11 16:11:09 +0300

    Select name and address

--- db.js
+++ db.js
@@ -9,9 +9,8 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers", function (err, result, fields) {
+    con.query("SELECT name, aadress FROM customers", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
     });
 });
-
